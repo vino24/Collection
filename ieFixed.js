10e2cae6b1c2e336d4084e850f6956961dda5059
vino <liugq19931023@gmail.com>
2016-02-28 19:39:03 +0800

    add Array.prototype.includes

--- ieFixed.js
+++ ieFixed.js
@@ -100,6 +100,32 @@ var ieFixed = {
 
                 return false;
             };
+            Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1]) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
             Array.prototype.every = function (fun /*, thisArg */) {
                 'use strict';
 
